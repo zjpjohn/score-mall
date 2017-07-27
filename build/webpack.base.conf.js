1ec079e79d4ec3e3c2f8ec4c44fee5823e0b7089
SophieU <j.u.sophia@qq.com>
2017-07-27 09:26:47 +0800

    修复webpack 设置resolve报错问题：原因（设置了空值）

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -19,11 +19,10 @@ module.exports = {
       : config.dev.assetsPublicPath
   },
   resolve: {
-    extensions: ['', '.js', '.vue', '.json'],
+    extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
-      'assets': resolve('src/assets')
     }
   },
   module: {
